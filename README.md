9dfcf69e7ad058a32765602142e55a0822738a52
Steve Penrod <github@gpll.org>
2017-10-30 03:35:33 -0700

    Web UI entry for AppID, code cleanup
    Added a settingsmeta.json value to allow a user to enter their own
    Wolfram Alpha AppID easily.  The old mycroft.conf setting is also
    still supported, but deprecated.
    
    Added support for 'Statement' responses if no 'Result' is found.
    
    Added comments throughout to make operation more clear.  Removed
    lots of 'dead code'.  Some might be restored later if the Wolfram
    Alpha proxy is brought back.

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# Wolfram Alpha Skill
+## Wolfram Alpha Fallback
+General knowledge fallback mechanism
 
-## Usage:
-* `what is the speed of the light`
-* `How tall is the Eiffel tower`
+## Description 
+This skill uses Wolfram Alpha as a source of general knowledge.  It requires an individual AppID from Wolfram Alpha (freely available to individuals at https://developer.wolframalpha.com).
+
+This skill can answer all sorts of questions, from factual to biographical.  Just ask it!
+
+## Examples 
+* "how tall is Mount Everest"
+* "when was The Rocky Horror Picture Show released"
+* "what is Madonna's real name"
+
+## Credits 
+Mycroft AI Inc
--- __init__.py
+++ __init__.py
@@ -37,10 +37,15 @@ class EnglishQuestionParser(object):
 
     def __init__(self):
         self.regexes = [
+            # Match things like:
+            #    * when X was Y, e.g. "tell me when america was founded"
+            #    how X is Y, e.g. "how tall is mount everest"
             re.compile(
                 ".*(?P<QuestionWord>who|what|when|where|why|which|whose) "
                 "(?P<Query1>.*) (?P<QuestionVerb>is|are|was|were) "
                 "(?P<Query2>.*)"),
+            # Match:
+            #    how X Y, e.g. "how do crickets chirp"
             re.compile(
                 ".*(?P<QuestionWord>who|what|when|where|why|which|how) "
                 "(?P<QuestionVerb>\w+) (?P<Query>.*)")
@@ -50,6 +55,7 @@ class EnglishQuestionParser(object):
         if 'Query' in groupdict:
             return groupdict
         elif 'Query1' and 'Query2' in groupdict:
+            # Join the two parts into a single 'Query'
             return {
                 'QuestionWord': groupdict.get('QuestionWord'),
                 'QuestionVerb': groupdict.get('QuestionVerb'),
@@ -66,73 +72,56 @@ class EnglishQuestionParser(object):
 
 
 class WolframAlphaSkill(FallbackSkill):
-    PIDS = ['Value', 'NotableFacts:PeopleData', 'BasicInformation:PeopleData',
-            'Definition', 'DecimalApproximation']
 
     def __init__(self):
         FallbackSkill.__init__(self, name="WolframAlphaSkill")
-        self.__init_client()
-        self.question_parser = EnglishQuestionParser()
 
-    def __init_client(self):
+        # Support 'api_key' in the old mycroft.conf location
         self.api_key = self.config.get('api_key')
 
+        # TODO: Localization support for questions in other languages
+        self.question_parser = EnglishQuestionParser()
+
     def initialize(self):
-        self.init_dialog(dirname(__file__))
         self.register_fallback(self.handle_fallback, 8)
 
-    def get_result(self, res):
-        try:
-            return next(res.results).text
-        except:
-            result = None
-            try:
-                for pid in self.PIDS:
-                    result = self.__find_pod_id(res.pods, pid)
-                    if result:
-                        result = result[:5]
-                        break
-                if not result:
-                    result = self.__find_num(res.pods, '200')
-                return result
-            except:
-                return result
-
-    # TODO: Localization
     def handle_fallback(self, message):
+        if not self.api_key:
+            # attempt to get from webUI
+            self.api_key = self.settings['api_key']
+            if not self.api_key:
+                # still not found, prompt user to get a key
+                self.speak_dialog("need.api.key")
+                return
+
         utt = message.data.get('utterance')
         LOG.debug("WolframAlpha fallback attempt: " + utt)
         lang = message.data.get('lang')
         if not lang:
             lang = "en-us"
 
+        # Convert things like "what's ..." to "what is ..."
         utterance = normalize(utt, lang)
         parsed_question = self.question_parser.parse(utterance)
 
-        query = utterance
-        others = []
         if parsed_question:
             # Try to store pieces of utterance (None if not parsed_question)
             utt_word = parsed_question.get('QuestionWord')
             utt_verb = parsed_question.get('QuestionVerb')
             utt_query = parsed_question.get('Query')
-            if utt_verb == "'s":
-                utt_verb = 'is'
-                parsed_question['QuestionVerb'] = 'is'
             query = "%s %s %s" % (utt_word, utt_verb, utt_query)
-            phrase = "know %s %s %s" % (utt_word, utt_query, utt_verb)
-            LOG.debug("Falling back to WolframAlpha: " + query)
         else:
             # This utterance doesn't look like a question, don't waste
             # time with WolframAlpha.
-
-            # TODO: Log missed intent
             LOG.debug("Unknown intent: " + utterance)
             return False
 
+        others = []
         try:
             self.enclosure.mouth_think()
 
+            # Query Wolfram Alpha directly
+            LOG.debug("Falling back to WolframAlpha: " + query)
             url = "https://api.wolframalpha.com/v2/query?input="
             url += query.replace(" ", "+")
             url += "&format=image,plaintext&output=JSON&appid="
@@ -142,120 +131,38 @@ class WolframAlphaSkill(FallbackSkill):
             resp = json.loads(res.content)
             if resp["queryresult"]["success"]:
                 # We got a result structure, interpret it
-                # self.get_result(resp["queryresult"]["pods"])
-                LOG.info("parsing resp")
                 str = ""
                 for pod in resp["queryresult"]["pods"]:
                     LOG.info("Pod title: "+pod["title"])
                     if pod["title"] == "Result":
                         str = pod["subpods"][0]["plaintext"]
 
-                str = re.sub("\(.*\)", "", str)
-                self.speak(str)
-                return True
-           
-            # res = self.client.query(query)
-            # SSP
+                if str == "":
+                    # If no 'Result', look for a 'Statement'
+                    for pod in resp["queryresult"]["pods"]:
+                        if pod["title"] == "Statement":
+                            str = pod["subpods"][0]["plaintext"]
 
-            # result = self.get_result(res)
-            if result is None:
-                others = self._find_did_you_mean(res)
-        except HTTPError as e:
-            if e.response.status_code == 401:
-                self.emitter.emit(Message("mycroft.not.paired"))
-            return True
+                # TODO: Look for unit conversion opportunities
+                # TODO: Help pronunciation, e.g. 6'3" as '6 foot 3 inches'
+
+                # Remove anything in parenthesis (assuming it is
+                # just parenthetical and not important)
+                str = re.sub("\(.*\)", "", str)
+
+                if str == "":
+                    return False;
+                else:
+                    self.speak(str)
+                    return True
         except Exception as e:
-            LOG.info("Exception caught")	
+            LOG.info("Exception caught")
             LOG.exception(e)
             return False
-
-        if result:
-            input_interpretation = self.__find_pod_id(res.pods, 'Input')
-            verb = "is"
-            structured_syntax_regex = re.compile(".*(\||\[|\\\\|\]).*")
-            if parsed_question:
-                if not input_interpretation or structured_syntax_regex.match(
-                        input_interpretation):
-                    input_interpretation = parsed_question.get('Query')
-                verb = parsed_question.get('QuestionVerb')
-
-            if "|" in result:  # Assuming "|" indicates a list of items
-                verb = ":"
-
-            LOG.info("Result: "+str(result))
-            result = self.process_wolfram_string(result)
-            LOG.debug("======== Asking: "+input_interpretation)
-            input_interpretation = \
-                self.process_wolfram_string(input_interpretation)
-
-            # This approach speaks portions of the question back.  The
-            # output is stilted and awkward, so commented out.
-            # response = "%s %s %s" % (input_interpretation, verb, result)
-            response = result
-            
-            self.speak(response)
-            return True
-        else:
-            if len(others) > 0:
-                self.speak_dialog('others.found',
-                                  data={'utterance': utterance,
-                                        'alternative': others[0]})
-                return True
-            else:
-                return False
-
-    @staticmethod
-    def __find_pod_id(pods, pod_id):
-        for pod in pods:
-            if pod_id in pod.id:
-                return pod.text
-        return None
-
-    @staticmethod
-    def __find_num(pods, pod_num):
-        for pod in pods:
-            if pod.node.attrib['position'] == pod_num:
-                return pod.text
-        return None
-
-    @staticmethod
-    def _find_did_you_mean(res):
-        value = []
-        root = res.tree.find('didyoumeans')
-        if root is not None:
-            for result in root:
-                value.append(result.text)
-        return value
-
-    def process_wolfram_string(self, text):
-        # Remove extra whitespace
-        text = re.sub(r" \s+", r" ", text)
-
-        # Convert | symbols to commas
-        text = re.sub(r" \| ", r", ", text)
-
-        # Convert newlines to commas
-        text = re.sub(r"\n", r", ", text)
-
-        # Convert !s to factorial
-        text = re.sub(r"!", r",factorial", text)
-
-        with open(join(dirname(__file__), 'regex',
-                       self.lang, 'list.rx'), 'r') as regex:
-            list_regex = re.compile(regex.readline())
-
-        match = list_regex.match(text)
-        if match:
-            text = match.group('Definition')
-
-        return text
 
     def shutdown(self):
         self.remove_fallback(self.handle_fallback)
         super(WolframAlphaSkill, self).shutdown()
-
-    def stop(self):
-        pass
 
 
 def create_skill():
